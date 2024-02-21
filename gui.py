dee3aa5d698889c1396919691bb77c75887d224c
unknown <rimeshlwagun10@gmail.com>
2024-02-21 09:01:10 +0545

    added next and skip function

--- gui.py
+++ gui.py
@@ -29,26 +29,70 @@ class LeftFrame(tk.Frame):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.load_directory_button = tk.Button(self, text="Open Image Directory", bg='#84a59d',relief='flat',command = self.load_directory_button_command)
-        self.load_image_directory = None 
 
-        # self.reset_checkpoint_button = tk.Button(self, text='ResetCheckpoint',bg='#C9ADA7',relief='flat', width = 15, command = self.reset_checkpoint_command)
-        self.reset_checkpoint = None 
+        self.load_directory_button = tk.Button(self.master, text="Open Image Directory", bg='#84a59d',relief='flat',command = self.load_directory_button_command)
+        self.load_image_directory = None  # assgin the actual statment to this variable
+
+        self.reset_checkpoint_button = tk.Button(self.master, text='Reset Checkpoint',bg='#C9ADA7',relief='flat', command = self.reset_checkpoint_command)
+        self.reset_checkpoint = None # assgin the actual statment to this variable
+
+        self.load_checkpoint_button = tk.Button(self.master, text='Load Checkpoint',bg='#C9ADA7',relief='flat', command = self.load_checkpoint_command) 
+        self.load_checkpoint = None # assgin the actual statment to this variable
+        
+        self.previous_button = tk.Button(self.master, text='<< Prev', width = 10,bg='#669BBC',relief='flat', command = self.previous_command)
+        self.previous = None # assgin the actual statment to this variable
+
+        self.skip_button = tk.Button(self.master, text ='Skip', width = 10,bg='#f28482',relief='flat', command = self.skip_command)
+        self.skip = None # assgin the actual statment to this variable
+
+        self.next_button = tk.Button(self.master, text='Next >>', width = 10,bg='#669BBC',relief='flat', command = self.next_command)
+        self.next = None # assgin the actual statment to this variable
 
         self.place_widgets()
 
     def place_widgets(self):
-        self.load_directory_button.grid(row=0, column=0, sticky=tk.W + tk.E, padx=5)
+        self.load_directory_button.grid(row=0, column=0, sticky=tk.W + tk.E, padx=5, pady=1)
+        self.reset_checkpoint_button.grid(row=1, column=0, sticky=tk.W + tk.E, padx=5, pady=1)
+        self.load_checkpoint_button.grid(row=2, column=0, sticky=tk.W + tk.E, padx=5, pady=1)
+        self.previous_button.grid(row=3 , column=0, sticky=tk.W + tk.E, padx = 5, pady = 1)
+        self.skip_button.grid(row=4 , column=0, sticky=tk.W + tk.E, padx = 5, pady = 1)
+        self.next_button.grid(row=5 , column=0, sticky=tk.W + tk.E, padx = 5, pady = 1)        
 
-    def load_directory_button_command(self, directory=None):
-        """
-        define your logic to load image directory
-        """
-        if self.load_image_directory:
-            self.load_image_directory()
-            return 
-        print('Left frame button condition not given')
+    def button_method_assign_warning(function_name):
+        print('in button method')
+        def decorator(function):
+            def wrapper(*args, **kwargs):
+                try:
+                    function(*args, **kwargs)
+                except Exception as e:
+                    print(f'assign your method to Left Frame Variable Name: {function_name}')
+            return wrapper
+        return decorator
+
+    @button_method_assign_warning('load_image_directory')
+    def load_directory_button_command(self, file_name: str = "chhabi"):
+        self.load_image_directory()
         ...
+
+    @button_method_assign_warning('reset_checkpoint')
+    def reset_checkpoint_command(self):
+        self.reset_checkpoint()
+
+    @button_method_assign_warning('load_checkpoint')
+    def load_checkpoint_command(self):
+        self.load_checkpoint()
+    
+    @button_method_assign_warning('previous')
+    def previous_command(self):
+        self.previous()
+        
+    @button_method_assign_warning('skip')
+    def skip_command(self):
+        self.skip()
+    
+    @button_method_assign_warning('next')
+    def next_command(self):
+        self.next()    
 
 class MenuBar(tk.Menu):
     # @inherit_signature_from(tk.Menu)
@@ -88,4 +132,7 @@ if __name__ == "__main__":
     menu_bar = MenuBar(master=root)
     root.config(menu=menu_bar)
     left_frame = LeftFrame(root)
+    left_frame.message = 'test'
+    print(left_frame.message, LeftFrame.message)
+    left_frame.msg()
     root.mainloop()
